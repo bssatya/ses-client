479e83cc50bb2bd8a0abd6785fd55a425e830a93
Jeff Jackson <jeffmjack@gmail.com>
2016-03-22 12:56:59 -0400

    get basic send_email working from ses sdk kit boto3

--- ses.py
+++ ses.py
@@ -2,5 +2,41 @@ import boto3
 
 client = boto3.client('ses')
 
+response = client.send_email(
+    Source='thegreencartkitchen@gmail.com',
+    Destination={
+        'ToAddresses': [
+            'jeffmjack@gmail.com',
+        ],
+#         'CcAddresses': [
+#            'string',
+#        ],
+#        'BccAddresses': [
+#            'string',
+#        ]
+    },
+    Message={
+        'Subject': {
+            'Data': 'hi there from Amazon SES',
+            'Charset': 'ascii'
+        },
+        'Body': {
+            'Text': {
+                'Data': 'hi, this is Jeff running a test for an email server',
+                'Charset': 'ascii'
+            },
+            'Html': {
+                'Data': 'looks like gmail or amazon defaults to using HTML in the message body',
+                'Charset': 'ascii'
+            }
+        }
+    },
+    ReplyToAddresses=[
+        'thegreencartkitchen@gmail.com',
+    ],
+    ReturnPath='jeffmjack@gmail.com',
+    SourceArn='arn:aws:ses:us-east-1:005005808680:identity/thegreencartkitchen@gmail.com',
+    ReturnPathArn='arn:aws:ses:us-east-1:005005808680:identity/jeffmjack@gmail.com'
+)
 
 
